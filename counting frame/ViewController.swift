24242110a168b8bdc814b407c565fe88b3fd0ff6
gf59ru <gf59ru@live.com>
2017-07-25 20:56:46 +0500

    Initial commit

---
+++ counting frame/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  counting frame
+//
+//  Created by Разработка on 25.07.17.
+//  Copyright © 2017 gf. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
